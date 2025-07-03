38adc63ad610e1a0f340dcff0f88445524e2b93a
Jesus Morales <morales.jesusant@gmail.com>
2025-07-03 16:52:19 -0700

    Added animation to Filler component.

--- src/pages/Home/homeComponents/Filler.jsx
+++ src/pages/Home/homeComponents/Filler.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
-import { motion } from 'framer-motion';
+import { motion, easeIn } from 'framer-motion';
 
 const Filler = () => {
+    const container = {
+        unMount: {y: 20, opacity: 0},
+        mount: {
+            y: 0, 
+            opacity: 1,
+            transition: {
+                duration: 2, 
+                ease: easeIn, 
+                staggerChildren: 0.8,
+            }
+        },
+    }
+
+    const child = {
+        unMount: {opacity: 0},
+        mount: {opacity: 1}
+    }
   return (
-    <motion.section className="filler">
-        <h4 className='award'>
+    <motion.section 
+        variants={container}
+        initial="unMount"
+        animate="mount"
+
+        className="filler">
+        <motion.h4 
+            variants={child}
+            className='award'>
             Award Winning
-        </h4>
-        <h2 className='experience'>
+        </motion.h4>
+        <motion.h2 
+            variants={child}
+            className='experience'>
             Experience the perfect blend of creativity and elegance in every frame.
-        </h2>
+        </motion.h2>
     </motion.section>
   )
 }
