feb78edeb1e774e70958357c96bf096dbf6ab108
Jesus Morales <morales.jesusant@gmail.com>
2025-07-06 01:54:18 -0700

    Minor fixes.

--- src/app/AppLayout.jsx
+++ src/app/AppLayout.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/layout/Navbar'
 import Footer from '../components/layout/Footer'
-import { motion } from 'framer-motion'
 
 const container = {
   unMount: {y: 15, opacity: 0},
@@ -35,23 +34,9 @@ const AppLayout = () => {
   return (
     <>
       <Navbar />
-      <motion.section 
-      initial={{opacity: 0}}
-      whileInView={{
-        opacity: 1,
-        transition: {
-          duration: 2, 
-          type: "spring", stiffness: 10,
-        }
-      }}
-      viewport={{
-        once: true,
-        amount: 0.5,
-      }}
-
-      className='main-container'>
+      <section className='main-container'>
         <Outlet context={{container, child}} />
-      </motion.section>
+      </section>
       <Footer />
     </>
   )
