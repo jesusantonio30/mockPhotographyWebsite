6f6bf90031f6d3fa631f3cd1a80bb4fdbffbc5e2
Jesus Morales <morales.jesusant@gmail.com>
2025-07-08 21:10:30 -0700

    Cleaned up styles.

--- src/pages/Home/homeComponents/Marketing.jsx
+++ src/pages/Home/homeComponents/Marketing.jsx
@@ -4,7 +4,7 @@ import Flower from '../../../assets/Icons/MarketingFlower.svg?react'
 
 const Marketing = () => {
   return (
-    <section className="marketing max-h-212.5 h-screen flex relative">
+    <section className="marketing">
         <div className='flex-3/4'
             style={{
                 backgroundImage: `url(${MarketingCouple})`,
@@ -12,10 +12,10 @@ const Marketing = () => {
                 backgroundPosition: 'center',
                 backgroundRepeat: 'no-repeat'
             }}/>
-        <div className="space flex-1/14" />
-        <div className='absolute z-10 top-33.5 right-11.5 w-147 h-145.25 bg-white opacity-90 flex flex-col items-center justify-center'>
+        <div className="marketing-space" />
+        <div className='marketing-content'>
             <Flower className="w-9.25"/>
-            <h4 className='font-vibes capitalize text-4xl py-4'>
+            <h4 className='marketing-font'>
                 Creative, Passionate, Artistic.
             </h4>
             <p className='max-w-5/10'>
